c903871320a5b7eff73ececad1fa740099b67135
Space Cookies <spacecookies.data@gmail.com>
2024-07-26 15:14:17 -0700

    parser commit

--- src/main/java/frc/robot/parsers/json/ClimberConfJson.java
+++ src/main/java/frc/robot/parsers/json/ClimberConfJson.java
@@ -2,7 +2,4 @@ package frc.robot.parsers.json;
 
 public class ClimberConfJson {
   public String elevatorFile;
-  public String rollerFile;
-  public String hooksFile;
-  public int breakBeamChannel;
 }
--- src/main/java/frc/robot/parsers/json/IndexerConfJson.java
+++ src/main/java/frc/robot/parsers/json/IndexerConfJson.java
@@ -2,10 +2,8 @@ package frc.robot.parsers.json;
 
 import frc.robot.parsers.json.utils.*;
 
-public class IntakeConfJson extends TagalongBaseJson {
+public class IndexerConfJson extends TagalongBaseJson {
   public String rollerFile;
   public String pivotFile;
   public String pivotConflict;
-  public PositionalLimitsJson pivotUnsafePositionalLimits;
-  public int breakBeamChannel;
 }
