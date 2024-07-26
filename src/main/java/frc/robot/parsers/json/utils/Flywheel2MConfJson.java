f2f65d6497df8806fc33d46fa22f115a31a55b0b
Space Cookies <spacecookies.data@gmail.com>
2024-07-26 16:07:36 -0700

    add single motor flywheel

--- src/main/java/frc/robot/parsers/json/utils/Flywheel2MConfJson.java
+++ src/main/java/frc/robot/parsers/json/utils/Flywheel2MConfJson.java
@@ -1,14 +1,16 @@
 package frc.robot.parsers.json.utils;
 
-public class FlywheelConfJson extends TagalongBaseJson {
+public class Flywheel2MConfJson extends TagalongBaseJson {
   public String name;
   public DeviceJson flywheelMotor;
+  public DeviceJson flywheelFollowerMotor;
 
   public int[][] gearRatio;
   public FeedForwardConstantsJson feedforward;
   public TrapezoidalLimitsJson trapezoidalLimits;
   public DefaultTolerancesJson defaultTolerances;
   public MotorControlJson flywheelMotorControl;
+  public MotorControlJson flywheelFollowerControl;
 
   private double calculatedGearRatio = -1;
 
