ca6e902576c73c99c8ca87bd9eb211300c6ead93
leonardosent <leonardosent@gmail.com>
2024-07-27 16:10:47 -0700

    Imported AprilTagSubsystem

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,8 +7,10 @@ package frc.robot;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveModule;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
 
-import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.State;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -26,93 +28,87 @@ import frc.robot.subsystems.NoteElevator;
 import frc.robot.subsystems.Shooter;
 
 public class RobotContainer {
-    public final static CommandSwerveDrivetrain commandSwerveDrivetrain = TunerConstants.DriveTrain; // My drivetrain
-    // NoteElevator noteElevator = new NoteElevator("configs/notevator/notevatorConf.json");
-    public static IndexerSubsystem indexerSubsystem = IndexerSubsystem.getInstance();
-    public static Shooter shooterSubsystem = Shooter.getInstance();
-    public static Climber climberSubsystem = Climber.getInstance();
-    public static NoteElevator noteElevator = NoteElevator.getInstance();
-    // driving in open loop
-    private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
-    private final PIDController driveRotationalPIDController = new PIDController(0.05, 0, 0);
     CommandXboxController commandXboxController = new CommandXboxController(Ports.DRIVER_XBOX_USB_PORT);
-    // The robot's subsystems and commands are defined here...
-    private double MaxSpeed = TunerConstants.kSpeedAt12VoltsMps; // kSpeedAt12VoltsMps desired top speed
-    private final Telemetry logger = new Telemetry(MaxSpeed);
-    private double MaxAngularRate = 1.5 * Math.PI; // 3/4 of a rotation per second max angular velocity
-    private final SwerveRequest.FieldCentric fieldCentricSwerveDrive = new SwerveRequest.FieldCentric()
-            .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
-            .withDriveRequestType(SwerveModule.DriveRequestType.OpenLoopVoltage); // I want field-centric
+  
+  public final static CommandSwerveDrivetrain commandSwerveDrivetrain = TunerConstants.DriveTrain; // My drivetrain
+  // driving in open loop
+  private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
+  // The robot's subsystems and commands are defined here...
+  private double MaxSpeed = TunerConstants.kSpeedAt12VoltsMps; // kSpeedAt12VoltsMps desired top speed
+  private final Telemetry logger = new Telemetry(MaxSpeed);
+  private double MaxAngularRate = 1.5 * Math.PI; // 3/4 of a rotation per second max angular velocity
+  private final SwerveRequest.FieldCentric fieldCentricSwerveDrive = new SwerveRequest.FieldCentric()
+          .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
+          .withDriveRequestType(SwerveModule.DriveRequestType.OpenLoopVoltage); // I want field-centric
+  private final TrapezoidProfile driveRotationalTrapezoidProfile = new TrapezoidProfile(new Constraints(18.9, 2.27));
+  public static IndexerSubsystem indexerSubsystem = IndexerSubsystem.getInstance();
+  public static Shooter shooterSubsystem = Shooter.getInstance();
+  public static Climber climberSubsystem = Climber.getInstance();
+  public static NoteElevator noteElevator = NoteElevator.getInstance();
 
     public RobotContainer() {
         configureBindings();
     }
 
     private void configureBindings() {
-        commandXboxController.rightTrigger().onTrue(IntakeCommands.intake());
-        commandXboxController.a().onTrue(IntakeCommands.switchAmpMode());
-        commandXboxController.b().onTrue(IntakeCommands.switchShooterMode());
+    commandSwerveDrivetrain.setDefaultCommand(// Drivetrain will execute this command periodically
+      new ConditionalCommand(
+        commandSwerveDrivetrain.applyRequest(() -> fieldCentricSwerveDrive.withVelocityX(commandXboxController.getLeftY() * MaxSpeed) // Drive forward with
+          // negative Y (forward)
+          .withVelocityY(commandXboxController.getLeftX() * MaxSpeed) // Drive left with negative X (left)
+          .withRotationalRate(commandXboxController.getRightX() * MaxAngularRate) // Drive counterclockwise with negative X (left)
+        ),
+        ScoreCommands.driveAutoTurn(commandXboxController, commandSwerveDrivetrain, fieldCentricSwerveDrive, driveRotationalTrapezoidProfile),
+        () -> commandXboxController.button(7).getAsBoolean()
+      )
+    );
+    
+    commandXboxController.rightTrigger().onTrue(IntakeCommands.intake());
+    commandXboxController.a().onTrue(IntakeCommands.switchAmpMode());
+    commandXboxController.b().onTrue(IntakeCommands.switchShooterMode());
 
-        commandXboxController.x().onTrue(ScoreCommands.ampScore());
+    commandXboxController.x().onTrue(ScoreCommands.ampScore());
 
-        commandXboxController.leftTrigger().onTrue(ScoreCommands.basicAutoShootCommand());
+    commandXboxController.leftTrigger().onTrue(ScoreCommands.basicAutoShootCommand());
 
-        commandSwerveDrivetrain.setDefaultCommand( // Drivetrain will execute this command periodically
-                commandSwerveDrivetrain.applyRequest(() -> fieldCentricSwerveDrive.withVelocityX(commandXboxController.getLeftY() * MaxSpeed) // Drive forward with
-                        // negative Y (forward)
-                        .withVelocityY(commandXboxController.getLeftX() * MaxSpeed) // Drive left with negative X (left)
-                        .withRotationalRate(commandXboxController.getRightX() * MaxAngularRate) // Drive counterclockwise with negative X (left)
-                )
-        );
-
-        commandXboxController.button(7).onTrue(
-                new ConditionalCommand(
-                        new InstantCommand(() -> commandSwerveDrivetrain.applyRequest(
-                                () -> fieldCentricSwerveDrive.withVelocityX(-commandXboxController.getLeftY()).withVelocityY(-commandXboxController.getLeftX())
-                                        .withRotationalRate(driveRotationalPIDController.calculate(commandSwerveDrivetrain.getPose().getRotation().getDegrees(), Units.radiansToDegrees(Math.atan((5.59 - commandSwerveDrivetrain.getPose().getY()) / (16.58 - commandSwerveDrivetrain.getPose().getX()))))))),
-                        new InstantCommand(() -> commandSwerveDrivetrain.applyRequest(
-                                () -> fieldCentricSwerveDrive.withVelocityX(-commandXboxController.getLeftY()).withVelocityY(-commandXboxController.getLeftX())
-                                        .withRotationalRate(driveRotationalPIDController.calculate((commandSwerveDrivetrain.getPose().getRotation().plus(Rotation2d.fromDegrees(180)).getDegrees()), Units.radiansToDegrees(Math.atan((5.48 - commandSwerveDrivetrain.getPose().getY()) / (-0.0381 - commandSwerveDrivetrain.getPose().getX()))))))),
-                        () -> DriverStation.getAlliance().get() == DriverStation.Alliance.Red
-                ));
-    }
+  }
 
     public Command getAutonomousCommand() {
-        return Commands.print("No autonomous command configured");
+      return Commands.print("No autonomous command configured");
     }
 
     public void onEnable() {
-        indexerSubsystem.onEnable();
-        shooterSubsystem.onEnable();
-        climberSubsystem.onEnable();
-        noteElevator.onEnable();
+      indexerSubsystem.onEnable();
+      shooterSubsystem.onEnable();
+      climberSubsystem.onEnable();
+      noteElevator.onEnable();
     }
 
     public void onDisable() {
-        indexerSubsystem.onDisable();
-        shooterSubsystem.onDisable();
-        climberSubsystem.onDisable();
-        noteElevator.onEnable();
+      indexerSubsystem.onDisable();
+      shooterSubsystem.onDisable();
+      climberSubsystem.onDisable();
+      noteElevator.onEnable();
     }
 
     public void disabledPeriodic() {
-        indexerSubsystem.disabledPeriodic();
-        shooterSubsystem.disabledPeriodic();
-        climberSubsystem.disabledPeriodic();
-        noteElevator.disabledPeriodic();
+      indexerSubsystem.disabledPeriodic();
+      shooterSubsystem.disabledPeriodic();
+      climberSubsystem.disabledPeriodic();
+      noteElevator.disabledPeriodic();
     }
 
     public void simulationInit() {
-        indexerSubsystem.simulationInit();
-        shooterSubsystem.simulationInit();
-        climberSubsystem.simulationInit();
-        noteElevator.simulationInit();
+      indexerSubsystem.simulationInit();
+      shooterSubsystem.simulationInit();
+      climberSubsystem.simulationInit();
+      noteElevator.simulationInit();
     }
 
     public void simulationPeriodic() {
-        indexerSubsystem.simulationPeriodic();
-        shooterSubsystem.simulationPeriodic();
-        climberSubsystem.simulationPeriodic();
-        noteElevator.simulationPeriodic();
+      indexerSubsystem.simulationPeriodic();
+      shooterSubsystem.simulationPeriodic();
+      climberSubsystem.simulationPeriodic();
+      noteElevator.simulationPeriodic();
     }
 }
--- src/main/java/frc/robot/commands/ScoreCommands.java
+++ src/main/java/frc/robot/commands/ScoreCommands.java
@@ -1,9 +1,23 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.*;
-import frc.robot.RobotContainer;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest.FieldCentric;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.State;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.ConditionalCommand;
+import edu.wpi.first.wpilibj2.command.FunctionalCommand;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.commands.base.ElevatorRaiseToCommand;
 import frc.robot.commands.base.PivotToCommand;
+import frc.robot.subsystems.CommandSwerveDrivetrain;
 import frc.robot.subsystems.IndexerSubsystem;
 import frc.robot.subsystems.NoteElevator;
 import frc.robot.subsystems.Shooter;
@@ -13,6 +27,22 @@ public class ScoreCommands {
     private static final IndexerSubsystem indexerSubsystem = IndexerSubsystem.getInstance();
     private static final NoteElevator elevatorSubsystem = NoteElevator.getInstance();
     private static final Shooter shooterSubsystem = Shooter.getInstance();
+    
+    public static Command driveAutoTurn(CommandXboxController commandXboxController, CommandSwerveDrivetrain commandSwerveDrivetrain, FieldCentric fieldCentricSwerveDrive, TrapezoidProfile driveRotationalTrapezoidProfile) {
+        return new ConditionalCommand(
+            new InstantCommand(() -> commandSwerveDrivetrain.applyRequest(
+              () -> fieldCentricSwerveDrive.withVelocityX(-commandXboxController.getLeftY()).withVelocityY(-commandXboxController.getLeftX())
+              .withRotationalRate(driveRotationalTrapezoidProfile.calculate(1.0,
+              new State(commandSwerveDrivetrain.getPose().getRotation().getDegrees(), commandSwerveDrivetrain.getPigeon2().getRate()),
+              new State(Units.radiansToDegrees(Math.atan((5.59 - commandSwerveDrivetrain.getPose().getY()) / (16.58 - commandSwerveDrivetrain.getPose().getX()))), 2.27)).velocity))),
+            new InstantCommand(() -> commandSwerveDrivetrain.applyRequest(
+              () -> fieldCentricSwerveDrive.withVelocityX(-commandXboxController.getLeftY()).withVelocityY(-commandXboxController.getLeftX())
+              .withRotationalRate(driveRotationalTrapezoidProfile.calculate(1.0,
+              (new State(commandSwerveDrivetrain.getPose().getRotation().plus(Rotation2d.fromDegrees(180)).getDegrees(), commandSwerveDrivetrain.getPigeon2().getRate())),
+              new State(Units.radiansToDegrees(Math.atan((5.48 - commandSwerveDrivetrain.getPose().getY()) / (-0.0381 - commandSwerveDrivetrain.getPose().getX()))), 2.27)).velocity))),
+            () -> DriverStation.getAlliance().get() == DriverStation.Alliance.Red
+        );
+    }
 
     public static Command elevatorStable() {
         return new ParallelCommandGroup(
