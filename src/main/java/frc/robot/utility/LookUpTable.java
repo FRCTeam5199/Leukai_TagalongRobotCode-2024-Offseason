c6c5c1a4a3d2b3766dcf448465a8475ff6d0022c
RohanP321 <120353864+Rohan3476@users.noreply.github.com>
2024-07-28 17:59:16 -0700

    added auto shoot commands

--- src/main/java/frc/robot/utility/LookUpTable.java
+++ src/main/java/frc/robot/utility/LookUpTable.java
@@ -1,30 +1,31 @@
 package frc.robot.utility;
 
+import edu.wpi.first.math.Pair;
+import frc.robot.commands.ShooterPivotAngles;
+
 import java.util.ArrayList;
 
 public class LookUpTable {
-    public static ArrayList<AutoAimValue> listOfValues = new ArrayList<>() {
+    public static ArrayList<Pair<Double, Double>> listOfValues = new ArrayList<>() {
         {
             //add AutoAimValues here
         }
     };
 
-    public static AutoAimValue findValue(double distance) {
-        AutoAimValue lowAutoAimValue = listOfValues.get(0);
-        AutoAimValue highAutoAimValue = listOfValues.get(listOfValues.size() - 1);
+    public static double findValue(double distance) {
+        Pair<Double, Double> lowAutoAimValue = listOfValues.get(0);
+        Pair<Double, Double> highAutoAimValue = listOfValues.get(listOfValues.size() - 1);
         for (int i = 0; i < listOfValues.size() - 1; i++) {
-            if (distance > listOfValues.get(i).distance && distance < listOfValues.get(i + 1).distance) {
+            if (distance > listOfValues.get(i).getFirst() && distance < listOfValues.get(i + 1).getFirst()) {
                 lowAutoAimValue = listOfValues.get(i);
                 highAutoAimValue = listOfValues.get(i + 1);
                 break;
             }
         }
 
-        double percentInBetween = (distance - lowAutoAimValue.distance) / (highAutoAimValue.distance - lowAutoAimValue.distance);
+        double percentInBetween = (distance - lowAutoAimValue.getFirst()) / (highAutoAimValue.getFirst() - lowAutoAimValue.getFirst());
 
-        double armAngle = lowAutoAimValue.armAngle - (percentInBetween * (lowAutoAimValue.armAngle - highAutoAimValue.armAngle));
-        double shooterRPM = lowAutoAimValue.shooterRPM + (percentInBetween * (highAutoAimValue.shooterRPM - lowAutoAimValue.shooterRPM));
-
-        return new AutoAimValue(distance, armAngle, shooterRPM);
+        return lowAutoAimValue.getSecond()
+                - (percentInBetween * (lowAutoAimValue.getSecond() - highAutoAimValue.getSecond()));
     }
 }
