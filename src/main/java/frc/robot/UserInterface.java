37721ccdb786d8c970ac59f96acc5039ebe852e2
leonardosent <leonardosent@gmail.com>
2024-08-06 16:52:52 -0700

    Added components

--- src/main/java/frc/robot/UserInterface.java
+++ src/main/java/frc/robot/UserInterface.java
@@ -1,20 +1,20 @@
 package frc.robot;
 
 import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.util.sendable.Sendable;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.robot.commands.Autos;
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.AmpTrapSubsystem;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 import frc.robot.subsystems.ShooterSubsystem;
+import frc.robot.tagalong.TagalongAngle;
 
 public class UserInterface {
     private static UserInterface userInterface;
-    
+
     private static final CommandSwerveDrivetrain commandSwerveDrivetrain = TunerConstants.DriveTrain;
     private static final ShooterSubsystem shooterSubsystem = ShooterSubsystem.getInstance();
     private static final AmpTrapSubsystem ampTrapSubsystem = AmpTrapSubsystem.getInstance();
@@ -22,11 +22,17 @@ public class UserInterface {
     private static final Field2d field2d = new Field2d();
 
     // Shuffleboard
-    private static ShuffleboardTab shuffleboardTestTab = Shuffleboard.getTab("Test");
-    private static GenericEntry shuffleboardEditModeComponent, shuffleboardShooterArmPositionComponent, shuffleboardShooterLeftFlywheelVelocityComponent, shuffleboardShooterRightFlywheelVelocityComponent, shuffleboardAmpTrapElevatorPositionComponent;
+    private static ShuffleboardTab shuffleboardRobotTab = Shuffleboard.getTab("Robot");
+    private static ShuffleboardTab shuffleboardSubsystemsTab = Shuffleboard.getTab("Subsystems");
+    private static ShuffleboardTab shuffleboardPreGameTab = Shuffleboard.getTab("Pre-Game");
+    private static ShuffleboardTab shuffleboardGameTab = Shuffleboard.getTab("Game");
 
-    private UserInterface() {
-    }
+    private static GenericEntry shuffleboardGyroComponent, shuffleboardDrivebaseComponent;
+    private static GenericEntry shuffleboardEditModeComponent, shuffleboardShooterArmPositionComponent, shuffleboardShooterLeftFlywheelVelocityComponent, shuffleboardShooterRightFlywheelVelocityComponent, shuffleboardShooterRightFlywheelFeedForwardComponent, shuffleboardShooterLeftFlywheelFeedForwardComponent, shuffleboardAmpTrapElevatorPositionComponent;
+    // private static GenericEntry shuffleboardEditModeComponent, shuffleboardShooterArmPositionComponent, shuffleboardShooterLeftFlywheelVelocityComponent, shuffleboardShooterRightFlywheelVelocityComponent, shuffleboardAmpTrapElevatorPositionComponent;
+    // private static GenericEntry shuffleboardEditModeComponent, shuffleboardShooterArmPositionComponent, shuffleboardShooterLeftFlywheelVelocityComponent, shuffleboardShooterRightFlywheelVelocityComponent, shuffleboardAmpTrapElevatorPositionComponent;
+
+    private UserInterface() {}
 
     public static UserInterface getInstance() {
         if (userInterface == null) userInterface = new UserInterface();
@@ -48,31 +54,50 @@ public class UserInterface {
     // private void initalizeData() {}
 
     private void initalizeWidgets() {
-        SmartDashboard.putData("Auto Selector", Autos.getInstance(commandSwerveDrivetrain).getAutoChooser());
-        SmartDashboard.putData("Field 2D", field2d);
+        // SmartDashboard.putData("Auto Selector", Autos.getInstance(commandSwerveDrivetrain).getAutoChooser());
+        // SmartDashboard.putData("Field 2D", field2d);
 
-        shuffleboardEditModeComponent = shuffleboardTestTab.add("Edit Mode", false)
+        // Drive Tab
+        shuffleboardGyroComponent = shuffleboardRobotTab.add("Gyro", 0)
+            .withWidget(BuiltInWidgets.kGyro)
+            .withPosition(0, 0)
+            .withSize(2, 2).getEntry();
+        shuffleboardDrivebaseComponent = shuffleboardRobotTab.add("Drivebase", commandSwerveDrivetrain)
+            .withWidget(BuiltInWidgets.kMecanumDrive)
+            .withPosition(0, 0)
+            .withSize(2, 2).getEntry();
+
+        // Subsystems Tab
+        shuffleboardEditModeComponent = shuffleboardSubsystemsTab.add("Edit Mode", false)
             .withWidget(BuiltInWidgets.kToggleSwitch)
             .withPosition(0, 0)
             .withSize(1, 1).getEntry();
 
 
-        shuffleboardShooterArmPositionComponent = shuffleboardTestTab.add("Shooter Arm Position", 0)
+        shuffleboardShooterArmPositionComponent = shuffleboardSubsystemsTab.add("Shooter Arm Position", 0)
             .withWidget(BuiltInWidgets.kTextView)
             .withPosition(1, 0)
             .withSize(1, 1).getEntry();
-        shuffleboardShooterLeftFlywheelVelocityComponent = shuffleboardTestTab.add("Shooter Left Flywheel Velocity", 0)
+        shuffleboardShooterLeftFlywheelVelocityComponent = shuffleboardSubsystemsTab.add("Shooter Left Flywheel Velocity", 0)
             .withWidget(BuiltInWidgets.kTextView)
             .withPosition(1, 1)
             .withSize(1, 1).getEntry();
-        shuffleboardShooterRightFlywheelVelocityComponent = shuffleboardTestTab.add("Shooter Right Flywheel Velocity", 0)
+        shuffleboardShooterRightFlywheelVelocityComponent = shuffleboardSubsystemsTab.add("Shooter Right Flywheel Velocity", 0)
             .withWidget(BuiltInWidgets.kTextView)
+            .withPosition(1, 2)
+            .withSize(1, 1).getEntry();
+        shuffleboardShooterRightFlywheelFeedForwardComponent = shuffleboardSubsystemsTab.add("Shooter Left Flywheel Feed Forward Enabled", 0)
+            .withWidget(BuiltInWidgets.kToggleButton)
             .withPosition(2, 1)
             .withSize(1, 1).getEntry();
+        shuffleboardShooterRightFlywheelFeedForwardComponent = shuffleboardSubsystemsTab.add("Shooter Left Flywheel Feed Forward Enabled", 0)
+            .withWidget(BuiltInWidgets.kToggleButton)
+            .withPosition(2, 2)
+            .withSize(1, 1).getEntry();
 
-        shuffleboardAmpTrapElevatorPositionComponent = shuffleboardTestTab.add("Amp/Trap Elevator Position", 0)
+        shuffleboardAmpTrapElevatorPositionComponent = shuffleboardSubsystemsTab.add("Amp/Trap Elevator Position", 0)
             .withWidget(BuiltInWidgets.kTextView)
-            .withPosition(2, 1)
+            .withPosition(3, 0)
             .withSize(1, 1).getEntry();
     }
 
@@ -83,11 +108,25 @@ public class UserInterface {
     public void updateWidgets() {
         field2d.setRobotPose(commandSwerveDrivetrain.getPose());
 
+        // Drive Tab
+        shuffleboardGyroComponent.setValue(commandSwerveDrivetrain.getPigeon2());
+        shuffleboardDrivebaseComponent.setValue(commandSwerveDrivetrain);
+
+        // Subsystems Tab
         if (!shuffleboardEditModeComponent.getBoolean(false)) {
             shuffleboardShooterArmPositionComponent.setDouble(shooterSubsystem.getPivot().getPivotPosition());
             shuffleboardShooterLeftFlywheelVelocityComponent.setDouble(shooterSubsystem.getFlywheel(0).getFlywheelVelocity());
             shuffleboardShooterRightFlywheelVelocityComponent.setDouble(shooterSubsystem.getFlywheel(1).getFlywheelVelocity());
-            shuffleboardAmpTrapElevatorPositionComponent.setDouble(ampTrapSubsystem.getElevator().getElevatorPosition());
+            shuffleboardShooterLeftFlywheelFeedForwardComponent.setDouble(shooterSubsystem.getFlywheel(0).getFlywheelVelocity());
+            shuffleboardShooterRightFlywheelFeedForwardComponent.setDouble(shooterSubsystem.getFlywheel(1).getFlywheelVelocity());
+
+            shuffleboardAmpTrapElevatorPositionComponent.setDouble(ampTrapSubsystem.getElevator().getElevatorHeightM());
+        } else {
+            shooterSubsystem.getPivot().setPivotProfile(shuffleboardShooterArmPositionComponent.getDouble(shooterSubsystem.getPivot().getPivotPosition()));
+            shooterSubsystem.getFlywheel(0).setFlywheelControl(shuffleboardShooterLeftFlywheelVelocityComponent.getDouble(shooterSubsystem.getFlywheel(0).getFlywheelVelocity()), shuffleboardShooterLeftFlywheelFeedForwardComponent.getBoolean(false));
+            shooterSubsystem.getFlywheel(1).setFlywheelControl(shuffleboardShooterRightFlywheelVelocityComponent.getDouble(shooterSubsystem.getFlywheel(1).getFlywheelVelocity()), shuffleboardShooterRightFlywheelFeedForwardComponent.getBoolean(false));
+
+            ampTrapSubsystem.getElevator().setElevatorHeight(shuffleboardAmpTrapElevatorPositionComponent.getDouble(ampTrapSubsystem.getElevator().getElevatorHeightM()));
         }
     }
 }
