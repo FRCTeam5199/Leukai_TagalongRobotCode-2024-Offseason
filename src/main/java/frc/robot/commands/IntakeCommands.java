64f300ef3b646e5a8cedf628ba537aa4383e76d4
Johnathon Reidel <jreidel8@gmail.com>
2024-11-18 17:45:35 -0800

    e[

--- src/main/java/frc/robot/commands/IntakeCommands.java
+++ src/main/java/frc/robot/commands/IntakeCommands.java
@@ -1,33 +1,40 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.commands;
+
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.subsystems.IndexerSubsystem;
-public class IntakeCommands{
-  IndexerSubsystem indexerSubsystem = IndexerSubsystem.getInstance();
-  public Command Intake() {
-    //TODO: tune?!
-    return indexerSubsystem.runOnce(
-        () -> {
-          indexerSubsystem.setRollerSpeeds(10,0,0);
-        });
-  }
-  public Command Amp() {
-    //TODO: tune?!
-    return indexerSubsystem.runOnce(
-        () -> {
-          indexerSubsystem.setRollerSpeeds(0,10,0);
-        });
-  }
-  public Command Indexer() {
-    //TODO: tune?!
-    return indexerSubsystem.runOnce(
-        () -> {
-          indexerSubsystem.setRollerSpeeds(0,0,10);
-        });
-  }
+import edu.wpi.first.wpilibj2.command.FunctionalCommand;
+import frc.robot.commands.base.ClimberHeights;
+import frc.robot.subsystems.AmpTrap;
+import frc.robot.constants.Constants;
+
+
+public class IntakeCommands {
+    /**
+     * Creates a new IntakeCommands.
+     */
+    private static AmpTrap elevatorSubsystem = AmpTrap.getInstance();
+
+
+    public static Command MoveElevatorToShoot() {
+        return new FunctionalCommand(
+                () -> {
+                },
+                () -> elevatorSubsystem.getElevator().setElevatorHeight(Constants.Amp.AMP_TOP_POSITiON),
+                interrupted -> elevatorSubsystem.getElevator().setHoldElevatorPosition(true),
+                () -> false,
+                elevatorSubsystem
+        );
+    }
+
+    public static Command MoveElevatorBack() {
+        return new FunctionalCommand(
+                () -> {
+                },
+                () -> elevatorSubsystem.getElevator().setElevatorHeight(Constants.Amp.AMP_START_POSITiON),
+                interrupted -> elevatorSubsystem.getElevator().setHoldElevatorPosition(true),
+                () -> false,
+                elevatorSubsystem
+        );
+    }
 
 }
+
