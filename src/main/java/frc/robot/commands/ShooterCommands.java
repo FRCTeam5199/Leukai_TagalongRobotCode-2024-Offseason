8dd49303274bfa71f819561a1922a6e6af716fc2
Gibriel <Shoukry.go@gmail.com>
2024-11-18 18:25:00 -0800

    Update ShooterCommands.java

--- src/main/java/frc/robot/commands/ShooterCommands.java
+++ src/main/java/frc/robot/commands/ShooterCommands.java
@@ -4,12 +4,13 @@
 
 package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.subsystems.ShooterSubsystem;;
+import frc.robot.subsystems.ShooterSubsystem;
 /** Add your docs here. */
 public class ShooterCommands {
     ShooterSubsystem shooterSubsystem = ShooterSubsystem.getInstance();
     public Command blankShoot() {
         return shooterSubsystem.runOnce(() -> {
+            //TODO: tune?
             shooterSubsystem.setShooterSpeeds(20);
         });
     }
