56b97ffe53d1ef91b9386984fc509e8548f55fba
Gibriel <Shoukry.go@gmail.com>
2024-12-02 19:31:27 -0800

    Update ShooterCommands.java
    the commands are there but no robot container yet

--- src/main/java/frc/robot/commands/ShooterCommands.java
+++ src/main/java/frc/robot/commands/ShooterCommands.java
@@ -7,34 +7,49 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 import frc.robot.commands.base.PivotToCommand;
+import frc.robot.subsystems.IndexerSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
 /** Add your docs here. */
 public class ShooterCommands {
+    /*
+     * Shoot 
+     */
+    static ShooterSubsystem shooterSubsystem = ShooterSubsystem.getInstance();
+    static IndexerSubsystem indexersubsystem = IndexerSubsystem.getInstance();
     public static Command blankShoot() {
         return new FunctionalCommand(
-            //TODO: tune?
-            ()->{},
-            ()->ShooterSubsystem.getInstance().setShooterSpeeds(20), 
-            (something)->{},
+            //TODO: don't tune?
+            ()->shooterSubsystem.setShooterSpeeds(60),
+            ()->{}, 
+            (something)->shooterSubsystem.setShooterSpeeds(0),
             ()->false,
-            ShooterSubsystem.getInstance()
+            shooterSubsystem
         );
     }
-    public static Command stopShooter() {
+    public static Command stablizeShooter() {
+        return new PivotToCommand<>(shooterSubsystem, ShooterPivotAngles.STABLE.getRotations(), true);
+    }
+    public static Command aimShooterLowShuttle() {
+        return new PivotToCommand<>(shooterSubsystem, ShooterPivotAngles.LOW_SHUTTlE.getRotations(), true);
+    }
+    public static Command aimShooterLow() {
+        return new PivotToCommand<>(shooterSubsystem, ShooterPivotAngles.LOW.getRotations(), true);
+    }
+    public static Command aimShooterMid() {
+        return new PivotToCommand<>(shooterSubsystem, ShooterPivotAngles.MID.getRotations(), true);
+    }
+    public static Command aimShooterHighShuttle() {
+        return new PivotToCommand<>(shooterSubsystem, ShooterPivotAngles.HIGH_SHUTTLE.getRotations(), true);
+    }
+    //50, -75, 50
+    public static Command moveIntoShooter() {
         return new FunctionalCommand(
-                //TODO: tune?
-                ()->{},
-                ()->ShooterSubsystem.getInstance().setShooterSpeeds(0),
-                (something)->{},
-                ()->true,
-                ShooterSubsystem.getInstance()
-        );
-    }
-    public static Command unAimShooter() {
-        return new PivotToCommand<>(ShooterSubsystem.getInstance(), ShooterPivotAngles.LOW.getRotations(), false);
-    }
-    public static Command aimShooterMid(boolean holdPos) {
-        return new PivotToCommand<>(ShooterSubsystem.getInstance(), ShooterPivotAngles.MID.getRotations(), holdPos);
+            () -> indexersubsystem.setRollerSpeeds(50, -75, 50),
+            () -> {},
+            (interrupted) -> indexersubsystem.setRollerSpeeds(0, 0, 0),
+            () -> false,
+            indexersubsystem
+            );
     }
 
     public static Command aimShooterClimb() {
