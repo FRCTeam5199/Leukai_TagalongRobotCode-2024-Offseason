6367d5c2d2b235c78343eaf52fcddc7bdcbcc73d
RohanP321 <111704710+RohanP321@users.noreply.github.com>
2024-12-08 13:10:05 -0800

    added encoder

--- src/main/java/frc/robot/templateSubsystems/TemplateSubsystem.java
+++ src/main/java/frc/robot/templateSubsystems/TemplateSubsystem.java
@@ -1,13 +1,14 @@
 package frc.robot.templateSubsystems;
 
+import com.ctre.phoenix6.configs.CANcoderConfiguration;
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.InvertedValue;
-import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.ctre.phoenix6.signals.*;
 import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.math.controller.ElevatorFeedforward;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
@@ -19,31 +20,41 @@ import frc.robot.utility.PID;
 
 public class TemplateSubsystem extends SubsystemBase {
     private final TalonFX motor;
-    private TalonFX followerMotor;
     private final TalonFXConfiguration motorConfig;
+
+    private TalonFX followerMotor;
     private Follower follower;
+
+    private CANcoder encoder;
+    private CANcoderConfiguration encoderConfig;
+
     private final TrapezoidProfile profile;
-    private final PositionVoltage positionVoltage;
-    private final VelocityVoltage velocityVoltage;
-    private final Slot0Configs slot0Configs;
-    private final double lowerTolerance;
-    private final double upperTolerance;
-    private final Timer timer;
-    private final Type type;
-    private SimpleMotorFeedforward simpleMotorFF;
-    private ElevatorFeedforward linearFF;
-    private ArmFeedforward pivotFF;
     private TrapezoidProfile.State currentState;
     private TrapezoidProfile.State goalState;
-    private double gearRatio = 1d;
-    private double drumCircumference;
-    private double mechMin;
-    private double mechMax;
     private double goal;
     private boolean followLastMechProfile = false;
 
+    private final PositionVoltage positionVoltage;
+    private final VelocityVoltage velocityVoltage;
+    private final Slot0Configs slot0Configs;
+
+    private SimpleMotorFeedforward simpleMotorFF;
+    private ElevatorFeedforward linearFF;
+    private ArmFeedforward pivotFF;
+
+    private final double lowerTolerance;
+    private final double upperTolerance;
+    private double mechMin;
+    private double mechMax;
+
+    private double gearRatio = 1d;
+    private double drumCircumference;
+
+    private final Timer timer;
+    private final Type type;
+
     public TemplateSubsystem(Type type, int id, TrapezoidProfile.Constraints constraints,
-                             PID pid, FeedForward feedforward,
+                             PID pid, FeedForward feedForward,
                              double lowerTolerance, double upperTolerance,
                              double[][] gearRatios) {
         this.type = type;
@@ -58,7 +69,14 @@ public class TemplateSubsystem extends SubsystemBase {
         slot0Configs = pid.getSlot0Configs();
         motor.getConfigurator().apply(slot0Configs);
 
-        setFeedForward(feedforward);
+        switch (type) {
+            case ROLLER, FLYWHEEL -> simpleMotorFF = new SimpleMotorFeedforward(
+                    feedForward.getkS(), feedForward.getkV(), feedForward.getkA());
+            case LINEAR -> linearFF = new ElevatorFeedforward(
+                    feedForward.getkS(), feedForward.getkG(), feedForward.getkV(), feedForward.getkA());
+            case PIVOT -> pivotFF = new ArmFeedforward(
+                    feedForward.getkS(), feedForward.getkG(), feedForward.getkV(), feedForward.getkA());
+        }
 
         positionVoltage = new PositionVoltage(0).withSlot(0).withEnableFOC(true);
         velocityVoltage = new VelocityVoltage(0).withSlot(0).withEnableFOC(true);
@@ -73,6 +91,7 @@ public class TemplateSubsystem extends SubsystemBase {
         timer = new Timer();
     }
 
+    //Configurations
     public void configureMotor(boolean isInverted, boolean isBrakeMode, double supplyCurrentLimit, double statorCurrentLimit) {
         motorConfig.MotorOutput.Inverted =
                 isInverted ? InvertedValue.Clockwise_Positive : InvertedValue.CounterClockwise_Positive;
@@ -81,18 +100,6 @@ public class TemplateSubsystem extends SubsystemBase {
         motorConfig.CurrentLimits.StatorCurrentLimit = statorCurrentLimit;
 
         motor.getConfigurator().apply(motorConfig);
-    }
-
-    //Configurations
-    private void setFeedForward(FeedForward feedForward) {
-        switch (type) {
-            case ROLLER, FLYWHEEL -> simpleMotorFF = new SimpleMotorFeedforward(
-                    feedForward.getkS(), feedForward.getkV(), feedForward.getkA());
-            case LINEAR -> linearFF = new ElevatorFeedforward(
-                    feedForward.getkS(), feedForward.getkG(), feedForward.getkV(), feedForward.getkA());
-            case PIVOT -> pivotFF = new ArmFeedforward(
-                    feedForward.getkS(), feedForward.getkG(), feedForward.getkV(), feedForward.getkA());
-        }
     }
 
     public void configureLinearMech(double drumCircumference, double mechMinM, double mechMaxM) {
@@ -112,8 +119,23 @@ public class TemplateSubsystem extends SubsystemBase {
         followerMotor.setControl(follower);
     }
 
-    public void configureEncoder() {
+    public void configureEncoder(int encoderId, String canbus, double magnetOffset,
+                                 double sensorToMechRatio, double motorToSensorRatio) {
+        encoder = new CANcoder(encoderId, canbus);
+        encoderConfig = new CANcoderConfiguration();
 
+        encoderConfig.MagnetSensor.AbsoluteSensorRange = AbsoluteSensorRangeValue.Unsigned_0To1;
+        encoderConfig.MagnetSensor.SensorDirection = SensorDirectionValue.Clockwise_Positive;
+        encoderConfig.MagnetSensor.MagnetOffset = magnetOffset;
+
+        encoder.getConfigurator().apply(encoderConfig);
+
+        motorConfig.Feedback.FeedbackRemoteSensorID = encoderId;
+        motorConfig.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.FusedCANcoder;
+        motorConfig.Feedback.SensorToMechanismRatio = sensorToMechRatio;
+        motorConfig.Feedback.RotorToSensorRatio = motorToSensorRatio;
+
+        motor.getConfigurator().apply(motorConfig);
     }
 
     //Unit Conversions
@@ -181,10 +203,10 @@ public class TemplateSubsystem extends SubsystemBase {
         return motor.getSupplyVoltage().getValueAsDouble();
     }
 
-
     public double getMechVelocity() {
         return getMechRotFromMotorRot(motor.getVelocity().getValueAsDouble());
     }
+
 
     public void setPercent(double percent) {
         motor.set(percent);
